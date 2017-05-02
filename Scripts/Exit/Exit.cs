571f96a50661564f28630a561c70b5c220f36644
Luca <luca.volturno@jcmaxwell.it>
2017-05-03 00:14:03 +0200

    Exit
    Script che permettono l'uscita o il ritorno al menù, dal videogioco

---
+++ Scripts/Exit/Exit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+	public void ReturnToStart()
+	{
+		SceneManager.LoadScene ("MenuPrincipale", LoadSceneMode.Single);
+	}
+
+	public void ReturnToWorld()
+	{
+		SceneManager.LoadScene ("World", LoadSceneMode.Single);
+	}
+
+	public void ExitWorld()
+	{
+		Application.Quit ();
+	}
+}
