01baa1bf4d178a880feb471498fc8f9def7ab5bd
Luca <luca.volturno@jcmaxwell.it>
2017-05-03 00:17:15 +0200

    Script del Menu Iniziale - StartMenu
    Script che permettono l'interazione dell'utente con il menu iniziale

---
+++ Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class StartMenu : MonoBehaviour {
+
+	public void EnterWorld()
+	{
+		SceneManager.LoadScene ("World", LoadSceneMode.Single);
+	}
+
+	public void ExitWorld()
+	{
+		SceneManager.LoadScene ("Exit", LoadSceneMode.Single);
+	}
+}
